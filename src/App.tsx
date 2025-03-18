c72ab25287866225b60babd4af966b12576b49a6
shotaro <850123.a@gmail.com>
2025-03-18 11:51:14 +0900

    32分のところまで

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { useSetAtom } from "jotai";
+import { notesAtom } from "./store";
+import { Note } from "./domain/note";
+import { useEffect } from "react";
+import SideMenu from "./components/SideMenu";
+import Editor from "./components/Editor";
+
 function App() {
+  const setNotes = useSetAtom(notesAtom);
+
+  useEffect(() => {
+    const noteData = [
+      new Note("1", "Note1", "Coontent 1", new Date().getTime()),
+      new Note("2", "Note2", "Coontent 2", new Date().getTime()),
+      new Note("3", "Note3", "Coontent 3", new Date().getTime()),
+    ]
+    setNotes(noteData);
+  }, []);
 
   return (
-   <>
-   <div>
-    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-      Button
-    </button>
-   </div>
-   </>
+    <>
+      <div className="flex h-screen w-full bg-white">
+        <SideMenu />
+        <Editor />
+      </div>
+    </>
   )
 }
 
--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,6 +1,28 @@
+import { useAtomValue } from "jotai"
+import { notesAtom } from "../store"
+
 function SideMenu() {
+  const notes = useAtomValue(notesAtom);
   return (
-    <div>SideMenu</div>
+    <div className="w-64 h-screen bg-gray-100 p-4 flex flex-col">
+      <div>
+        <h2>Notes</h2>
+        <button>+</button>
+      </div>
+      <div>
+        {notes.map((note) => (
+          <div key={note.id} className="p-2 mb-2 rounded cursor-pointer flex justify-between items-center group">
+            <div className="flex-1 min-w-0">
+              <input type="text" className="bg-gray-100" value={note.title} />
+              <p>
+                {note.lastEditTime ? new Date(note.lastEditTime).toLocaleDateString() : "Never edited"}
+              </p>
+            </div>
+            <button>-</button>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
